3050eeec8b292fa5998adceb3f8fa97b90e1b131
tlawlals112 <tlawlals112@gmail.com>
2025-03-20 21:20:37 +0900

    file directory edited: tank.js, level_manager.js

--- assets/models/tank/level_manager.js
+++
@@ -1,8 +0,0 @@
-// level_manager.js
-export function createTank(scene) {
-  BABYLON.SceneLoader.ImportMeshAsync("", "./assets/models/tank/", "tank.glb", scene).then(result => {
-      const tank = result.meshes[0];
-      tank.scaling = new BABYLON.Vector3(0.1, 0.1, 0.1); // 크기 조절
-      return tank;
-  });
-}
